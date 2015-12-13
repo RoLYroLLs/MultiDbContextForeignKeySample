d568b51e976176bb1188884006f43e1c61507e35
RoLYroLLs <github@rolyrolls.com>
2015-12-13 16:49:31 -0500

    added ClientDbContext

---
+++ MultiDbContextForeignKeySample/DataContexts/ClientDb.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+namespace MultiDbContextForeignKeySample.DataContexts {
+	public class ClientDbContext : DbContext {
+		public ClientDbContext()
+			: base("DefaultConnection") { }
+
+		public DbSet<Models.Clients> Clients { get; set; }
+	}
+}
